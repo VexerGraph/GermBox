957f4f09ee67497dfc19c0042e070d677e821160
VexerGraph <vexergraph@gmail.com>
2025-09-19 18:36:20 -0600

    Add project files.

---
+++ ModClass.cs
@@ -0,0 +1,12 @@
+﻿using NeoModLoader.api;
+
+namespace GermBox
+{
+    public class ModClass : BasicMod<ModClass>
+    {
+        protected override void OnModLoad()
+        {
+            
+        }
+    }
+}
