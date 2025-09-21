f3dab9a026bce35d73beac2c05f88f0ba99089f1
VexerGraph <vexergraph@gmail.com>
2025-09-20 23:15:40 -0600

    Modified actor death patch to remove pathogen and its associated status when there are no units left infected by the disease.

--- Patches/ActorDeath.cs
+++ Patches/ActorDeath.cs
@@ -18,11 +18,21 @@ namespace GermBox.Patches
             Pathogen pathogen = PathogenManager.GetPathogenById(__instance.id);
             if (pathogen != null) {
                 pathogen.Stats.infected--;
-                //if(pathogen.Stats.infected == 0)
-                //{
-                //    PathogenManager.pathogens.Remove(pathogen);
-                //}
-                //probably best we don't remove it since this can cause statuses to overwrite themselves
+                if (pathogen.Stats.infected == 0)
+                {
+                    StatusAsset status = AssetManager.status.get(pathogen.Id());
+
+                    for (int i = 0; i < AssetManager.status.list.Count; i++) {
+                        if (AssetManager.status.list[i].id == status.id)
+                        {
+                           AssetManager.status.list.RemoveAt(i);
+                            break;
+                        }
+                        AssetManager.status.dict.Remove(status.id);
+                    }
+
+                    PathogenManager.pathogens.Remove(pathogen);
+                }
             }
         }
     }
